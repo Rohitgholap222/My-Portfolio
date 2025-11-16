0934830a14e5e8e6b216ba724ef8926981ac49fb
rohitgholap172 <rohitgholap045@gmail.com>
2025-11-16 15:30:41 +0530

    routes set for all pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,20 @@
 import Home from "./home/Home";
 import Projects from "./home/Projects";
+import Skills from "./home/Skills";
+import About from "./home/About";
+import Contact from "./home/Contact";
+import NotFound from "./home/NotFound";
+import Resume from "./home/Resume";
+import Certificates from "./home/Certificates";
+
 
 export {
     Home,
-    Projects
+    Projects,
+    Skills,
+    About,
+    Contact,
+    NotFound,
+    Resume,
+    Certificates
 };
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -5,6 +5,12 @@ const router = createBrowserRouter([
     
     {   path : "/", element : <pages.Home/>    },
     {   path : "/projects", element : <pages.Projects/>    },
+    {   path : "/skills", element : <pages.Skills/>    },
+    {   path : "/certificate", element : <pages.Certificate/>    },
+    {   path : "/about", element : <pages.About/>    },
+    {   path : "/contact", element : <pages.Contact/>    },
+    {   path : "/resume", element : <pages.Resume/>    },
+    {   path : "*", element : <pages.NotFound/>    },
 ])
 
 export default router;
